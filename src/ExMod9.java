a4bf06186d81fae9b99a4f4c6ccc8f55f7b249d1
phvalverde <phvalverde.dev@gmail.com>
2022-10-21 17:47:20 -0300

    Primeiro commit

---
+++ src/ExMod9.java
@@ -0,0 +1,19 @@
+public class ExMod9 {
+    public static void main(String[] args) {
+        Exercicio();
+    }
+
+    private static void Exercicio() {
+
+        int a = 10;
+        System.out.println(a);
+        Integer b = a;
+        System.out.println(b);
+
+        double c = 10.2;
+        System.out.println(c);
+        Double d = c;
+        System.out.println(d);
+
+    }
+}
